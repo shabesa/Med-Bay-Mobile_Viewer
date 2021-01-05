7db230562d9b73d05bf6aab57222884f9f51acb0
k.a.shabesa <54354603+shabesa@users.noreply.github.com>
2021-01-05 11:22:45 +0530

    added Change Url Login Page View

---
+++ Med-Bay/app/src/main/java/com/example/med_bay/UrlLoginPage.java
@@ -0,0 +1,14 @@
+package com.example.med_bay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UrlLoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_url_login_page);
+    }
+}
