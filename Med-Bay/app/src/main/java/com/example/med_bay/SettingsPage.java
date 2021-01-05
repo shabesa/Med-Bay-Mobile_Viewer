76f2fa7de076a88acb33d51373bdbdad23c840f6
k.a.shabesa <54354603+shabesa@users.noreply.github.com>
2021-01-05 11:59:17 +0530

    added Change Url Page View and linking codes

--- Med-Bay/app/src/main/java/com/example/med_bay/SettingsPage.java
+++ Med-Bay/app/src/main/java/com/example/med_bay/SettingsPage.java
@@ -2,13 +2,30 @@ package com.example.med_bay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class SettingsPage extends AppCompatActivity {
+
+    Button ChangeUrl;
+    Button About;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_page);
+
+        ChangeUrl = (Button) findViewById(R.id.ChangeUrlButton);
+        About = (Button) findViewById(R.id.AboutButton);
+
+        ChangeUrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingsPage.this, UrlLoginPage.class));
+            }
+        });
     }
 }
--- Med-Bay/app/src/main/java/com/example/med_bay/UrlLoginPage.java
+++ Med-Bay/app/src/main/java/com/example/med_bay/UrlLoginPage.java
@@ -2,13 +2,36 @@ package com.example.med_bay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UrlLoginPage extends AppCompatActivity {
+
+    EditText UsernameInput;
+    EditText PasswordInput;
+    Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_url_login_page);
+
+        UsernameInput = (EditText) findViewById(R.id.UsernameField);
+        PasswordInput = (EditText) findViewById(R.id.PasswordField);
+        Login = (Button) findViewById(R.id.LoginButton);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (UsernameInput.getText().toString().equals("Admin") && PasswordInput.getText().toString().equals("Admin!23")){
+                    startActivity(new Intent(UrlLoginPage.this, ChangeUrlPage.class));
+                }
+            }
+        });
+
+
     }
 }
